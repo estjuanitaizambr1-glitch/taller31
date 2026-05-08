5252e3dfef891bdec15c05fba3fff98f4faad19b
estjuanitaizambr1-glitch <est.juanitai.zambr1@unimilitar.edu.co>
2026-05-08 09:27:47 -0500

    Implementar funcion viewport

--- script.js
+++ script.js
@@ -1,2 +1,15 @@
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
+
+function dibujarViewport(xmin, ymin, xmax, ymax){
+
+    ctx.strokeStyle = "black";
+    ctx.lineWidth = 2;
+
+    ctx.strokeRect(
+        xmin,
+        ymin,
+        xmax - xmin,
+        ymax - ymin
+    );
+}
