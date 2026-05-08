254332cedb0a930ba95bb4e4dec2b901c98c205e
estjuanitaizambr1-glitch <est.juanitai.zambr1@unimilitar.edu.co>
2026-05-08 09:29:42 -0500

    Implementar funcion de dibujo de lineas

--- script.js
+++ script.js
@@ -13,3 +13,17 @@ function dibujarViewport(xmin, ymin, xmax, ymax){
         ymax - ymin
     );
 }
+function dibujarLinea(x1, y1, x2, y2, color){
+
+    ctx.beginPath();
+
+    ctx.moveTo(x1, y1);
+
+    ctx.lineTo(x2, y2);
+
+    ctx.strokeStyle = color;
+
+    ctx.lineWidth = 3;
+
+    ctx.stroke();
+}
